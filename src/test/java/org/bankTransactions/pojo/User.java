9f1de2cd3e9399d64134db5a1dcf9c2f0e1fd84f
jordan.gonzalez <jordan.olaves91@gmail.com>
2022-11-22 21:05:09 -0500

    pojo added

---
+++ src/test/java/org/bankTransactions/pojo/User.java
@@ -0,0 +1,14 @@
+package org.bankTransactions.pojo;
+
+public class User {
+    private int id;
+    private String name;
+    private String lastName;
+    private int accountNumber;
+    private float amount;
+    private int transactionType;
+    private String email;
+    private boolean active;
+    private String country;
+    private int telephone;
+}
